cd5973524933448c89e51c8bca202952136eac25
radigurev <radi.gurev@gmail.com>
2022-06-29 07:59:04 +0300

    still working on it

---
+++ watchDog/Program.cs
@@ -0,0 +1,45 @@
+﻿using WatchDog.Service;
+using System.Timers;
+
+HttpClient client = new HttpClient();
+
+var timer = new System.Timers.Timer()
+{
+    Interval = (int)(1 * 1 * 1000)
+};
+
+
+EmailService email = new EmailService();
+
+email.emailForWebsiteDownIsSent = false;
+
+timer.Elapsed += Timer_Elapsed;
+
+
+
+async void Timer_Elapsed(object? sender, ElapsedEventArgs e)
+{
+    var response = await client.GetAsync("http://proekt7.eu");
+
+    if (!response.IsSuccessStatusCode)
+    {
+        if (!email.emailForWebsiteDownIsSent)
+        {
+            Console.WriteLine("neba4ka");
+            //email.SendEmailForDownServer();
+            email.emailForWebsiteDownIsSent = true;
+        }
+    }
+    else
+    {
+        if (email.emailForWebsiteDownIsSent)
+        {
+            Console.WriteLine("ba4ka");
+            //email.SendEmailForDownServer();
+            email.emailForWebsiteDownIsSent = false;
+        }
+    }
+
+}
+timer.Start();
+await Task.Delay(-1);
---
+++ watchDog/Service/EmailService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Net;
+
+namespace WatchDog.Service
+{
+
+    class EmailService
+    {
+        public bool emailForWebsiteDownIsSent;
+
+        const string from = "smc.developer@outlook.com";
+        const string to = "rg00749943@edu.mon.bg";
+
+        const string username = "smc.developer@outlook.com";
+        const string password = "EQu6aD,{m.";
+
+        const string host = "smtp.office365.com";
+        const int port = 587;
+
+        public void SendEmailForDownServer()
+        {
+            var EmailClient = new SmtpClient(host, port)
+            {
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(username, password),
+                EnableSsl = true
+            };
+
+            EmailClient.Send(from, to, "Test", "Test");
+        }
+
+        public void SendEmailForUpServer()
+        {
+            var EmailClient = new SmtpClient(host, port)
+            {
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(username, password),
+                EnableSsl = true
+            };
+
+            EmailClient.Send(from, to, "Test", "Test");
+        }
+
+    }
+}
